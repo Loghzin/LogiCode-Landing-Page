2f720f59c4b4e7f24c3b04581e76a58526f18b4e
Logh <lolapaludaokok@gmail.com>
2025-08-26 20:59:54 -0300

    .

---
+++ LogiCode/logicode/src/main/java/com/frontend/logicode/LogicodeApplication.java
@@ -0,0 +1,13 @@
+package com.frontend.logicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicodeApplication.class, args);
+	}
+
+}
